8d1b253756f57298dd9f4691e81accf35a16ae8b
League Student <student@jointheleague.org>
2026-07-02 12:45:01 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/30_Turtle_Tricks/10_Turtle_Tricks.py
+++ lessons/10_Turtles/30_Turtle_Tricks/10_Turtle_Tricks.py
@@ -19,9 +19,9 @@ tina = turtle.Turtle()                  # Create a turtle named tina
 # tina.pencolor()
 
 tina.forward(10)
-tina.pencolor(blue)
+tina.pencolor("blue")
 tina.left(10)
-tina.pencolor(green)
+tina.pencolor("green")
 tina.forward(10)
 
 turtle.exitonclick()                    # Close the window when we click on it
