931702f618d3b33e63970b203a317cd3276f0794
League Student <student@jointheleague.org>
2026-07-02 13:08:01 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/50_Variables_and_Functions/20_Efficient_Turtle.py
+++ lessons/10_Turtles/50_Variables_and_Functions/20_Efficient_Turtle.py
@@ -5,7 +5,7 @@ In this program, use what you've learned about functions and variables to make a
 
 - Create a function that draws a polygon based on the number of sides passed to it as an argument.
 - Use variables to calculate the angle needed to turn the turtle based on the number of sides.
-- Call the function multiple times with different arguments to draw a square, pentagon, and hexagon.
+- Call the function multiple times with different arguments to draw a square, pentagon, and hexagon.gvtfrc47<?|
 """
 
 import turtle                            # Tell Python we want to work with the turtle
