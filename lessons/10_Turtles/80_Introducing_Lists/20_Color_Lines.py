e57a590634e4629744ffb984a393aed7a182577e
League Student <student@jointheleague.org>
2026-07-07 12:58:04 -0700

    Automated commit

--- lessons/10_Turtles/80_Introducing_Lists/20_Color_Lines.py
+++ lessons/10_Turtles/80_Introducing_Lists/20_Color_Lines.py
@@ -1,26 +1,24 @@
 """
 # 20_Color_Lines.py
 
-import turtle
+Finish the program to make Tina draw a square with each side being a different color.
+"""
 
-# Tell Python we want to work with the turtle
-turtle.setup(600, 600, 0, 0) # Set the size of the window
-tina = turtle.Turtle() # Create a turtle named tina
-tina.shape('turtle') # Set the shape of the turtle to a turtle
-tina.speed(2) # Move at a moderate speed, not too fast.
+import turtle                           # Tell Python we want to work with the turtle
+turtle.setup(600, 600, 0, 0)            # Set the size of the window
 
-colors = ['red', 'blue', 'black', 'orange'] # define a list of colors
+tina = turtle.Turtle()                  # Create a turtle named tina
 
-# 1) Make the first square
-for color in colors: # loop through the colors
-    tina.pencolor(color) # Set the pen color for the side
-    tina.forward(100) # Move forward 100 pixels
-    tina.left(90) # Turn left 90 degrees
+tina.shape('turtle')                    # Set the shape of the turtle to a turtle
+tina.speed(2)                           # Move at a moderate speed, not too fast.
 
-# 2) Make another square, but put the colors in reverse order
-for i in range(-1, -5, -1): # Loop backwards using negative indices (-1, -2, -3, -4)
-    tina.pencolor(colors[i]) # Set the pen color to the reverse index
-    tina.forward(100) # Move forward 100 pixels
-    tina.left(90) # Turn left 90 degrees
+colors = [ 'red', 'blue', 'black', 'orange']    # define a list of colors
 
-turtle.exitonclick() # Close the window when we click on it
+for color in colors:                            # loop through the colors
+    ... # Your code here
+
+# 2) Make another square, but put the colors in reverse order, using a negative index. 
+
+... # Your code here
+
+turtle.exitonclick()                     # Close the window when we click on it
