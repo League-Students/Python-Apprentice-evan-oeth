fe67ac090bdc15119ec765f2159615ccc384da1f
League Student <student@jointheleague.org>
2026-07-07 13:01:01 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/90_Graphics_Projects/20_Crazy_Spiral.py
+++ lessons/10_Turtles/90_Graphics_Projects/20_Crazy_Spiral.py
@@ -1,32 +1,34 @@
-"""
-# 20_Crazy_Spiral.py
+import turtle
 
-Make your own crazy spiral with a pattern like
-in 10_Flaming_Ninja_Star.py, but use what you've learned about loops
+# Set up the window
+turtle.setup(800, 800, 0, 0)
+turtle.bgcolor("black")  # Dark background makes the crazy spiral pop!
 
-uid: zfzMbyH7
-name: Crazy Spiral
-"""
+t = turtle.Turtle()  # Create a turtle named t
+t.shape("turtle")
+t.speed(0)  # Set to fastest speed so it draws quickly
+t.width(2)
 
-... # Copy code to make a turtle and set up the window
+# List of vibrant colors for the crazy pattern
+colors = ["red", "purple", "blue", "green", "orange", "yellow"]
 
-t = ... # Create a turtle named t
 
-# 1) Complete make_a_shape() to make the turtle move in some pattern. 
-# For instance, you can make it go left 30 degrees, then forward 50 pixels, 
-# then right 60 degrees, then forward 100 pixels. Make any shape you like.
-
+# 1) Complete make_a_shape() to make the turtle move in some pattern.
 def make_a_shape(t):
-    """Make a shape with turtle t. Make it go left or right or forward"""    
-    ...
+    """Make a sharp, asymmetric star-like segment with turtle t."""
+    t.forward(120)
+    t.left(40)
+    t.forward(40)
+    t.right(130)
+    t.forward(50)
+    t.left(30)
+
 
 # 2) Call make_a_shape() in a loop to make the turtle draw a spiral.
-# For instance, you can call make_a_shape() 100 times to make a spiral with 100 shapes.
-# The second ... in the for loop should be the number of shapes you want to make,
-# for example 100, or a list of numbers.
+num_shapes = 120  # Draw 120 shapes to make a dense, overlapping spiral
 
-num_shapes = ...
-
-for i in range(...):
+for i in range(num_shapes):t.pencolor(colors[i % len(colors)])  # Cycle through the list of colors
     make_a_shape(t)
-    t.right(360/num_shapes)
+    t.right(360 / num_shapes + 2)  # Adding '+ 2' creates the cascading spiral effect
+
+turtle.exitonclick()
