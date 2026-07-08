cc05e34dee9c55d7838f655ab2501b38fc72d46c
League Student <student@jointheleague.org>
2026-07-08 12:42:17 -0700

    Automated commit

--- Fractal.py
+++ Fractal.py
@@ -5,7 +5,7 @@ screen = turtle.screen
 screen.setup(600, 600)
 
 def fractal_triangle(size,depth):
-    if depth == 0:
+    if depth == 0: 
         for i in range(3):
             tina.forward(size)
             tina.left(120)
